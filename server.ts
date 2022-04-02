9b5c7762283f672e64fb707e5563ab15be82d74b
Tejas-3098 <47113471+Tejas-3098@users.noreply.github.com>
2022-04-02 19:05:30 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -46,8 +46,8 @@ let sess = {
     saveUninitialized: true,
     resave: true,
     cookie: {
-        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.ENVIRONMENT === "PRODUCTION" ? 'none' : 'lax',
+        secure: process.env.ENVIRONMENT === "PRODUCTION",
     }
 }
 
