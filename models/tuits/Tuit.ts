18654803d981f5ab510cd74e5b6e5ed09bc2a640
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-24 15:36:53 -0400

    Added jsdoc for Tuit.js

--- models/tuits/Tuit.ts
+++ models/tuits/Tuit.ts
@@ -1,6 +1,22 @@
+/**
+ * @file Declares Tuit data type representing a tuit posted by the user. A tuit can be liked and bookmarked
+ * by various users.
+ */
 import User from "../users/User";
 import Stats from "./Stats";
 
+/**
+ * @typedef Tuit Represents a Tuit posted by a user
+ *
+ * @property {string} tuit the content of the tuit being posted
+ * @property {Date} postedOn the date when the tuit was posted
+ * @property {User} postedBy User posting the tuit
+ * @property {string} image the content of the tuit being posted
+ * @property {string} youtube the date when the tuit was posted
+ * @property {string} avatarLogo User posting the tuit
+ * @property {string} imageOverlay the content of the tuit being posted
+ * @property {Stats} stats the date when the tuit was posted
+*/
 export default interface Tuit {
     tuit: string,
     postedBy: User,
