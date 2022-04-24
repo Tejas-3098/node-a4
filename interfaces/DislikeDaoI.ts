e0a637b4c92c7c9bb8365de77e2f557dc517b11e
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-24 15:33:52 -0400

    Update DislikeDaoI.ts

--- interfaces/DislikeDaoI.ts
+++ interfaces/DislikeDaoI.ts
@@ -7,4 +7,5 @@ export default interface DislikeDaoI {
     findAllUsersThatDislikedTuit (tid: string): Promise<Dislike[]>;
     findAllTuitsDislikedByUser (uid: string): Promise<Dislike[]>;
     userDislikesTuit (tid: string, uid: string): Promise<Dislike>;
+    userUndislikesTuit (tid: string, uid: string): Promise<any>;
 }
