435c10de471099261f9689ab22216f7564adccd9
Tejas Sridhar <47113471+Tejas-3098@users.noreply.github.com>
2022-04-24 15:42:16 -0400

    Dislike button backend code

--- mongoose/tuits/TuitSchema.ts
+++ mongoose/tuits/TuitSchema.ts
@@ -1,6 +1,10 @@
+/**
+ * @file Defines mongoose schema for documents in the tuits collection
+ */
 import mongoose, {Schema} from "mongoose";
-import Tuit from "../../models/tuits/Tuit";
+
 const TuitSchema = new mongoose.Schema<Tuit>({
+    
     tuit: {type: String, required: true},
     postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
     postedOn: {type: Date, default: Date.now},
@@ -11,7 +15,11 @@ const TuitSchema = new mongoose.Schema<Tuit>({
     stats: {
         replies: {type: Number, default: 0},
         retuits: {type: Number, default: 0},
-        likes: {type: Number, default: 0}
+        likes: {type: Number, default: 0},
+        dislikes: {type: Number, default: 0},
+        currentUserLike: {type: Number, default: 0},
+        currentUserLike: {type: Number, default: 0}
     }
+
 }, {collection: "tuits"});
 export default TuitSchema;
